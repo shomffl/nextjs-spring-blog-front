a99adc6d743b6ff382b68b93783d19e07d30ef63
sho watanabe <showatanabe0226@gmail.com>
2023-01-11 21:12:51 +0900

    [update]headerとfooterのレイアウトを修正

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -6,7 +6,7 @@ export const Layout = ({ children }: any) => {
       <header className="p-6 w-full bg-purple-800 fixed top-0 text-4xl font-bold text-gray-200">
         TEST APP
       </header>
-      <main className="pt-24 pb-20 ">{children}</main>
+      <main className="pt-28 pb-20 px-10">{children}</main>
       <footer className="p-5 w-full bg-gray-800 fixed bottom-0 text-center text-xs text-white">
         ©️2023 shomffl
       </footer>
